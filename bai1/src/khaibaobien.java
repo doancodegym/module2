fb58840e0cf00cd7c9eb553d1e97dc20f1c57a40
doancodegym <nguyenhongdoan14@gmailcom>
2020-04-08 13:13:07 +0700

    thuc hanh bai 1

---
+++ bai1/src/khaibaobien.java
@@ -0,0 +1,16 @@
+public class khaibaobien {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 25.5f;
+        double d = 25.5;
+        boolean b = true;
+        char a = 'a';
+        String s = "welcome";
+        System.out.println("i : "+i);
+        System.out.println("f : "+f);
+        System.out.println("d : "+d);
+        System.out.println("b : "+b);
+        System.out.println("a : "+a);
+        System.out.println("s : "+s);
+    }
+}
